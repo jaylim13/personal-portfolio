0588c87172f2d267863f39df81e3933f09ba95e4
Jayden Lim <jaylim@unc.edu>
2023-09-29 03:07:32 -0400

    3rd commit with General page updates

--- src/pages/General.js
+++ src/pages/General.js
@@ -1,7 +1,18 @@
 export default function General(){
     return (
     <><h1>General</h1>
-    <h2>About Me</h2></>
+    <h2>About Me</h2>
+    <img src="images/jaylim.jpg" alt="Jayden after a haircut" />
+    <div className="container">
+        <h3>Short Bio</h3>
+        <p className="bio">What's up! My name is Jayden, and I am a second year Computer Science student at UNC
+            Chapel Hill! I am currently learning React, HTML, CSS, and Javascript. Email 
+            me to stay connected!
+        </p>
+        <pre><a href="mailto:jaydenlim43@gmail.com">Email Me</a></pre>
+        <a href="https://www.linkedin.com/in/jayden-lim-438b66232" target="_blank" rel="noreferrer">LinkedIn</a>
+    </div>
+    </>
     )
         
 }
